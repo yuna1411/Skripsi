e88791efeb6837cebe57275b5367a61b0039c252
yuna1411 <89563587+yuna1411@users.noreply.github.com>
2022-07-15 14:16:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,34 @@ from apps import home, data # import your app modules here
 
 app = MultiApp()
 
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
 st.markdown("""
-# Aplikasi Rekomendasi Produk Skin Care
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://yuna1411-skripsi-home-lpkjyw.streamlitapp.com/" target="_blank">Get Recommendation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Data Insight</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+st.markdown('''# **Aplikasi Rekomendasi Produk Skin Care**
+Selamat Datang di Dunia Skin Care ^-^  
+''')
 
-""")
-
-# Add all your application here
-app.add_app("Rekomendasi Produk Skin Care", home.app)
-app.add_app("Data Insight", data.app)
 
 # The main app
 app.run()
