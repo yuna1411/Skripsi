580deb471952a0ad06c2bf7d7538db8fc14a8a33
yuna1411 <89563587+yuna1411@users.noreply.github.com>
2022-07-15 16:02:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def streamlit_menu(example=1):
 selected = streamlit_menu(example=EXAMPLE_NO)
 
 if selected == "Skin Care":
-    st.title(f"Aplikasi Rekomendasi Produk Skin Care {selected}" :sparkles:)
+    st.title(f"Aplikasi Rekomendasi Produk Skin Care {selected} :sparkles:")
     st.write('---') 
 
     st.write(
