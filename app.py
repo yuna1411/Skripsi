7bc5827f2c5ff1a9205a5e62188420c15ba1656a
yuna1411 <89563587+yuna1411@users.noreply.github.com>
2022-07-15 15:46:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
-EXAMPLE_NO = 1
+EXAMPLE_NO = 3
 
 
 def streamlit_menu(example=3):
@@ -58,6 +58,8 @@ selected = streamlit_menu(example=EXAMPLE_NO)
 
 if selected == "Skin Care":
     st.title(f"Aplikasi Rekomendasi Produk {selected}")
+    
+    
 if selected == "Get Recommendation":
     st.title(f"Let's {selected}")
 if selected == "Data Insight":
