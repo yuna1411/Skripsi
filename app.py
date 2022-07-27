e89f75c6defa8536842e8b75e3da0d580fe64a20
yuna1411 <89563587+yuna1411@users.noreply.github.com>
2022-07-27 22:42:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from PIL import image
+from PIL import Image
 
 
 # Import the Dataset 
